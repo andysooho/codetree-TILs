8c6bc44249d68481ae5309ed61c0ab49df9d7961
soooh <71422211+andysooho@users.noreply.github.com>
2023-11-12 23:37:07 +0900

    Title: 데이터 비교 2, Time: 454ms, Memory: 47MB, Status: Passed - CodeTree

--- 231112/데이터 비교 2/date-comparison-2.py
+++ 231112/데이터 비교 2/date-comparison-2.py
@@ -2,11 +2,13 @@ import sys
 from collections import Counter
 
 n = int(input())
-arr = [sys.stdin.readline().rstrip() for _ in range(2*n-1)]
+arr = [sys.stdin.readline().rstrip() for _ in range(n)]
+arr2 = [sys.stdin.readline().rstrip() for _ in range(n-1)]
 
 count = Counter(arr)
+count2 = Counter(arr2)
 
-for i in count:
-    if count[i] == 1:
+for i in count-count2:
+    if count[i] != 0:
         print(i)
         break
