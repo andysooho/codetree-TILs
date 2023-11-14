a4cd3d90baf7336a80c33ea1292a591cde4a25c8
soooh <71422211+andysooho@users.noreply.github.com>
2023-11-14 22:41:25 +0900

    Title: T 제거하기, Time: 1468ms, Memory: 33MB, Status: Passed - CodeTree

--- 231114/T 제거하기/remove-t.py
+++ 231114/T 제거하기/remove-t.py
@@ -1,29 +1,13 @@
-s = input()
+string = input()
 t = input()
 
-t_hash = hash(t)
+stack = []
 
-def simple_rabin_carp(s):
-    global t, t_hash
-    stack = []
-    i=0
-    while i < len(s):
-        if s[i] != t[0]:
-            stack.append(s[i])
-            i += 1
-        elif hash(s[i:i+len(t)]) == t_hash:
-            i += len(t)
-        else:
-            stack.append(s[i])
-            i += 1
+for c in string:
+    stack.append(c)
+    if len(stack) >= len(t):
+        if ''.join(stack[-len(t):]) == t:
+            for _ in range(len(t)):
+                stack.pop()
 
-    return ''.join(stack)
-
-
-answer = simple_rabin_carp(s)
-while True:
-    before_answer = hash(answer)
-    answer = simple_rabin_carp(answer)
-    if before_answer == hash(answer):
-        print(answer)
-        break
+print(''.join(stack))
