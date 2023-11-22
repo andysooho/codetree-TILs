ed579b1925999f1836ce91539fc77624e57a526d
soooh <71422211+andysooho@users.noreply.github.com>
2023-11-23 00:29:24 +0900

    Title: 소수, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231123/소수/prime-number.py
@@ -0,0 +1,22 @@
+a,b = map(int,input().split())
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+count = 0
+min_val = 1e9
+for num in range(a,b+1):
+    if is_prime(num):
+        count += num
+        min_val = min(min_val, num)
+
+if not count:
+    print(0)
+    exit()
+print(count)
+print(min_val)
