fc27c99cc050d0d62666dacb9e16def12f48699c
soooh <71422211+andysooho@users.noreply.github.com>
2023-11-23 10:18:41 +0900

    Title: 문자 채워넣기, Time: 257ms, Memory: 68MB, Status: Passed - CodeTree

--- 231123/문자 채워넣기/fill-in-text.py
+++ 231123/문자 채워넣기/fill-in-text.py
@@ -1,9 +1,13 @@
+from collections import defaultdict
+
 n = int(input())
 s = list(map(str,input().split()))
 
-count = 0
-for i in range(n-1):
-    if s[i] != s[i+1]:
-        count += 1
+count = defaultdict(lambda:0)
+count[s[0]] = 1
 
-print(count)
+for i in range(1,n):
+    if s[i] != s[i-1]:
+        count[s[i]] += 1
+
+print(1+min(count['a'],count['b']))
